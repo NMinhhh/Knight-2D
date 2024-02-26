44fc6b9f60ad6e6277924fc23259f7749c9d7ffc
Nhat Minh <leminh2020ct@gmail.com>
2024-02-26 10:33:13 +0700

    update shoting and delete file shotgun and bombgun

--- Assets/Scripts/Weapons/PlayerShooting.cs
+++ Assets/Scripts/Weapons/PlayerShooting.cs
@@ -9,7 +9,7 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField] private WeaponObject data;
 
     [Header("Shoting")]
-    [SerializeField] private Transform attackPoint;
+    [SerializeField] private Transform[] attackPoint;
 
     [Header("Cooldown")]
     private float timer;
@@ -22,7 +22,11 @@ public class PlayerShooting : MonoBehaviour
     [Header("Sound")]
     [SerializeField] private AudioClip clip;
 
-    [SerializeField] private HandleRotation handleRotation;
+
+    [Header("Type Of Weapon")]
+    [SerializeField] private bool gun;
+    [SerializeField] private bool shotGun;
+    [SerializeField] private bool grenadeLauncher;
 
     private void Start()
     {
@@ -32,10 +36,15 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        Shooting();
+        if(gun)
+            ShootingGun();
+        else if(shotGun)
+            ShootingShotGun();
+        else
+            ShootingBomb();
     }
 
-    void Shooting()
+    void ShootingGun()
     {
         timer += Time.deltaTime;
         if(InputManager.Instance.shoting && timer >= data.cooldown && reloadBullets.amountOfBullet > 0)
@@ -43,20 +52,66 @@ public class PlayerShooting : MonoBehaviour
             timer = 0; 
             Vector3 localScale = Vector3.one;
             anim.SetTrigger("shoot");
-            SoundFXManager.Instance.CreateAudioClip(clip, attackPoint, .5f);
-            GameObject GO = Instantiate(data.bulletIcon, attackPoint.position, attackPoint.rotation);
-            if (handleRotation.angle > 90 || handleRotation.angle < -90)
-            {
-                localScale.y = -1f;
-            }
-            else
-            {
-                localScale.y = 1f;
-            }
-            GO.transform.localScale = localScale;
-            Projectile script = GO.GetComponent<Projectile>();
-            script.CreateBullet(data.damage, data.speed, data.timeLife);
+            SoundFXManager.Instance.CreateAudioClip(clip, attackPoint[0], .5f);
+            SpawnBullet(attackPoint[0], attackPoint[0].rotation);
+            //GameObject GO = Instantiate(data.bulletIcon, attackPoint[0].position, attackPoint[0].rotation);
+            //if (handleRotation.angle > 90 || handleRotation.angle < -90)
+            //{
+            //    localScale.y = -1f;
+            //}
+            //else
+            //{
+            //    localScale.y = 1f;
+            //}
+            //GO.transform.localScale = localScale;
+            //Projectile script = GO.GetComponent<Projectile>();
+            //script.CreateBullet(data.damage, data.speed, data.timeLife);
             reloadBullets.UpdateBullets();  
         }
+    }
+
+    void ShootingShotGun()
+    {
+        timer += Time.deltaTime;
+        if (InputManager.Instance.shoting && timer >= data.cooldown && reloadBullets.amountOfBullet > 0)
+        {
+            timer = 0;
+            anim.SetTrigger("shoot");
+            SoundFXManager.Instance.CreateAudioClip(clip, attackPoint[0], .5f);
+            foreach (Transform pos in attackPoint)
+            {
+                SpawnBullet(pos, pos.rotation);
+            }
+            reloadBullets.UpdateBullets();
+        }
+
+    }
+
+    void ShootingBomb()
+    {
+        timer += Time.deltaTime;
+        if (InputManager.Instance.shoting && timer >= data.cooldown && reloadBullets.amountOfBullet > 0)
+        {
+            timer = 0;
+            anim.SetTrigger("shoot");
+            SoundFXManager.Instance.CreateAudioClip(clip, attackPoint[0], .5f);
+            SpawnRocket(attackPoint[0], attackPoint[0].rotation);
+            reloadBullets.UpdateBullets();
+        }
+
+    }
+
+    void SpawnBullet(Transform spawnPos, Quaternion ro)
+    {
+        GameObject projectile = Instantiate(data.bulletIcon, spawnPos.position, ro);
+        Projectile script = projectile.GetComponent<Projectile>();
+        script.CreateBullet(data.damage, data.speed, data.timeLife);
+    }
+
+    void SpawnRocket(Transform spawnPos, Quaternion ro)
+    {
+        GameObject projectile = Instantiate(data.bulletIcon, spawnPos.position, ro);
+        ProjectileBomb script = projectile.GetComponent<ProjectileBomb>();
+        script.CreateBomb(data.damage, data.speed, data.timeLife);
     }
 }
